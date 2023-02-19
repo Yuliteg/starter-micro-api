b9c837fcf346fea7dd6438485143ba71ed5201f9
Yulia <78503488+Yuliteg@users.noreply.github.com>
2023-02-19 13:14:58 +0100

    init

---
+++ models/UserContactDetails.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserContactDetailsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Item = mongoose.model(
+  'UserContactDetails',
+  UserContactDetailsSchema
+);
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const UserContactDetails = require('./models/UserContactDetails');
+
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const dbConnection = mongoose.connection;
+dbConnection.on('open', () => {
+  console.log('DB connected');
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api/contact-form', async (req, res) => {
+  try {
+    const newUserContactDetails = new UserContactDetails({
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+    });
+
+    const result = await newUserContactDetails.save();
+    res.json(result);
+  } catch (error) {
+    res.send('Error ' + err);
+  }
+});
+
+app.get('/api/contact-form', async (req, res) => {
+  try {
+    const contactDetails = await UserContactDetails.find();
+    res.json(contactDetails);
+  } catch (error) {
+    res.send('Error ' + err);
+  }
+});
+
+const port = 5000;
+app.listen(process.env.PORT || 5000, () => console.log('Server running...'));
